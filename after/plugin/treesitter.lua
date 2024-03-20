1d40c8b2bc62082f2dda8aa196454ffacd68e59e
030820acc <030820acc@gmail.com>
2024-03-19 23:33:55 -0500

    added new langauge parsers to the config file for treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = {"html", "css", "svelte", "python", "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = {"html", "css", "svelte", "python", "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query", "zig", "rust", "sql", "ruby", "java", "haskell", "go", "cpp", "c_sharp", "bash" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
